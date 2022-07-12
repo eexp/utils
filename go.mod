09f39445f3ff69874595467f907814b2a22227cb
M09Ic <m09ician@gmail.com>
2022-07-13 00:01:33 +0800

    init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/chainreactors/ipcs
+
+go 1.17
+
+require github.com/chainreactors/logs v0.3.2
+
+require github.com/chainreactors/files v0.1.2 // indirect
---
+++ utils.go
@@ -0,0 +1,23 @@
+package icps
+
+import "strconv"
+
+func SliceUnique(ss []string) []string {
+	res := make([]string, 0, len(ss))
+	temp := map[string]struct{}{}
+	for _, item := range ss {
+		if _, ok := temp[item]; !ok {
+			temp[item] = struct{}{}
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
+func ToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return i
+}
