b5ec455f0c58c77c00e5173a3a3087c46e5a8aec
M09Ic <m09ician@gmail.com>
2022-07-16 00:53:02 +0800

    新增大量常用ip,cidr api

--- cidr.go
+++ cidr.go
@@ -2,10 +2,27 @@ package icps
 
 import (
 	"fmt"
+	"math"
+	"net"
+	"sort"
 	"strings"
 )
 
-func ParseCIDR(target string) (string, error) {
+func SplitCIDR(cidr string) (string, int) {
+	tmp := strings.Split(cidr, "/")
+	if len(tmp) == 2 {
+		return tmp[0], toInt(tmp[1])
+	} else {
+		return tmp[0], 32
+	}
+}
+
+func NewCIDR(ip string, mask int) *CIDR {
+	i, _ := ParseIP(ip)
+	return &CIDR{*i, mask}
+}
+
+func ParseCIDR(target string) (*CIDR, error) {
 	// return ip, hosts
 	var ip string
 	var mask int
@@ -18,17 +35,114 @@ func ParseCIDR(target string) (string, error) {
 	}
 
 	if parsedIp, err := ParseIP(ip); err == nil {
-		return fmt.Sprintf("%s:%s", parsedIp, mask), nil
+		return &CIDR{*parsedIp, mask}, nil
 	} else {
-		return "", err
+		return nil, err
 	}
 }
 
-func SplitCIDR(cidr string) (string, int) {
-	tmp := strings.Split(cidr, "/")
-	if len(tmp) == 2 {
-		return tmp[0], toInt(tmp[1])
+type CIDR struct {
+	IP   IP
+	Mask int
+}
+
+func (c CIDR) String() string {
+	return fmt.Sprintf("%s/%d", c.IP.String(), c.Mask)
+}
+
+func (c CIDR) IPString() string {
+	return c.IP.String()
+}
+
+func (c CIDR) FirstIP() *IP {
+	return NewIPWithInt(c.IP.Int() & MaskToIPInt(c.Mask))
+}
+
+func (c CIDR) LastIP() *IP {
+	return NewIPWithInt(c.IP.Int() | ^MaskToIPInt(c.Mask))
+}
+
+func (c CIDR) Net() *net.IPNet {
+	return &net.IPNet{c.IP.IP, net.CIDRMask(c.Mask, 32)}
+}
+
+func (c CIDR) NetWithMask(mask int) *net.IPNet {
+	return &net.IPNet{c.IP.IP, net.CIDRMask(mask, 32)}
+}
+
+func (c CIDR) IPMask() net.IPMask {
+	return net.CIDRMask(c.Mask, 32)
+}
+
+func (c CIDR) Count() uint {
+	return 1 << (32 - c.Mask)
+}
+
+func (c CIDR) Range() (first, final uint) {
+	first = c.IP.Int()
+	final = first | uint(math.Pow(2, float64(32-c.Mask))-1)
+	return first, final
+}
+
+func (c CIDR) RangeIP() (first, final uint) {
+	first = c.IP.Int()
+	final = first | uint(math.Pow(2, float64(32-c.Mask))-1)
+	return first, final
+}
+
+func (c CIDR) ContainsCIDR(cidr CIDR) bool {
+	return c.Net().Contains(cidr.IP.IP)
+}
+
+func (c CIDR) ContainsIP(ip IP) bool {
+	return c.Net().Contains(ip.IP)
+}
+
+type CIDRs []CIDR
+
+func (cs CIDRs) Less(i, j int) bool {
+	ipi := cs[i].FirstIP().Int()
+	ipj := cs[j].FirstIP().Int()
+	if ipi == ipj {
+		if cs[i].Mask < cs[j].Mask {
+			return true
+		} else {
+			return false
+		}
+	} else if ipi < ipj {
+		return true
 	} else {
-		return tmp[0], 32
+		return false
 	}
+}
+
+func (cs CIDRs) Swap(i, j int) {
+	cs[i], cs[j] = cs[j], cs[i]
+}
+
+func (cs CIDRs) Len() int {
+	return len(cs)
+}
+
+func (cs CIDRs) Coalesce() CIDRs {
+	sort.Sort(cs)
+	var newCIDRs CIDRs
+	for i := 0; i < len(cs)-1; i++ {
+		j := i
+		for j < len(cs)-1 {
+			if !cs[j].ContainsCIDR(cs[j+1]) {
+				break
+			} else {
+				j++
+			}
+		}
+		cs[i].IP = *cs[i].FirstIP()
+		newCIDRs = append(newCIDRs, cs[i])
+		i = j
+	}
+	return newCIDRs
+}
+
+func MaskToIPInt(mask int) uint {
+	return uint(((uint64(4294967296) >> uint(32-mask)) - 1) << uint(32-mask))
 }
--- ip.go
+++ ip.go
@@ -4,35 +4,15 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
-	"sort"
 	"strings"
 )
 
-func IsIPv4(ip string) bool {
-	address := net.ParseIP(ip).To4()
-	if address != nil {
+func IsIpv4(ip string) bool {
+	net.ParseIP(ip).To4()
+	if net.ParseIP(ip).To4() != nil {
 		return true
 	}
 	return false
-}
-
-func ParseIP(target string) (string, error) {
-	target = strings.TrimSpace(target)
-	if IsIPv4(target) {
-		return target, nil
-	}
-	iprecords, err := net.LookupIP(target)
-	if err != nil {
-		return "", fmt.Errorf("Unable to resolve domain name:" + target + ". SKIPPED!")
-	}
-
-	for _, ip := range iprecords {
-		if ip.To4() != nil {
-			//Log.Important("parse domain SUCCESS, map " + target + " to " + ip.String())
-			return ip.String(), nil
-		}
-	}
-	return "", fmt.Errorf("not found Ip address")
 }
 
 func Ip2Int(ip string) uint {
@@ -45,9 +25,105 @@ func Int2Ip(ipint uint) string {
 	return ip.String()
 }
 
-func SortIP(ips []string) []string {
-	sort.Slice(ips, func(i, j int) bool {
-		return Ip2Int(ips[i]) < Ip2Int(ips[j])
-	})
-	return ips
+func NewIPWithInt(ipint uint) *IP {
+	return &IP{IP: net.IP{byte(ipint >> 24), byte(ipint >> 16), byte(ipint >> 8), byte(ipint)}.To4()}
+}
+
+func ParseIP(target string) (*IP, error) {
+	target = strings.TrimSpace(target)
+	if IsIpv4(target) {
+		return &IP{IP: net.ParseIP(target).To4()}, nil
+	}
+
+	iprecords, err := net.LookupIP(target)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to resolve domain name:" + target + ". SKIPPED!")
+	}
+
+	for _, ip := range iprecords {
+		if ip.To4() != nil {
+			//Log.Important("parse domain SUCCESS, map " + target + " to " + ip.String())
+			return &IP{ip.To4(), target}, nil
+		}
+	}
+	return nil, fmt.Errorf("not found Ip address")
+}
+
+type IP struct {
+	IP   net.IP
+	Host string
+}
+
+//func (ip IP) IsIPv4() bool {
+//	if ip.IP.To4() != nil {
+//		return true
+//	}
+//	return false
+//}
+
+func (ip IP) Int() uint {
+	return uint(binary.BigEndian.Uint32(ip.IP.To4()))
+}
+
+func (ip IP) String() string {
+	return ip.IP.To4().String()
+}
+
+func (ip IP) Mask(mask int) IP {
+	return IP{IP: ip.IP.Mask(net.CIDRMask(mask, 32))}
+}
+
+type IPs []IP
+
+func (is IPs) Less(i, j int) bool {
+	ipi := is[i].Int()
+	ipj := is[j].Int()
+	if ipi < ipj {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (is IPs) Swap(i, j int) {
+	is[i], is[j] = is[j], is[i]
+}
+
+func (is IPs) Len() int {
+	return len(is)
+}
+
+func (is IPs) Approx() CIDRs {
+	cidrMap := make(map[string]*CIDR)
+
+	for _, ip := range is {
+		fmt.Println(ip.String())
+		if n, ok := cidrMap[ip.Mask(24).String()]; ok {
+			var baseNet byte
+			var nowN, newN byte
+			for i := 8; i > 0; i-- {
+				nowN = n.IP.IP[3] & (1 << (i - 1)) >> (i - 1)
+				newN = ip.IP[3] & (1 << (i - 1)) >> (i - 1)
+				if nowN&newN == 1 {
+					baseNet += 1 << (i - 1)
+				}
+				if nowN^newN == 1 {
+					n.Mask = 32 - i
+					n.IP.IP[3] = baseNet
+					break
+				}
+			}
+		} else {
+			cidrMap[ip.Mask(24).String()] = &CIDR{ip, 32}
+		}
+	}
+
+	approxed := make(CIDRs, len(cidrMap))
+	var index int
+	for _, cidr := range cidrMap {
+		approxed[index] = *cidr
+		index++
+	}
+
+	return approxed
 }
